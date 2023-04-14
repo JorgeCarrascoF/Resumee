b02b6fd7a743112acbf866976df80ea9388ba77b
JorgeCarrascoF <105206644+JorgeCarrascoF@users.noreply.github.com>
2023-04-14 09:46:25 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+
+const hover = document.getElementById("hover");
+let i = 0;
+
+const click = document.getElementById("click");
+
+phone.addEventListener('click', ()=>{copy(phone)});
+email.addEventListener('click', ()=>{copy(email)});
+
+// TODO: add sound and mute button, MAKE RESPONSIVE
+
+function copy(item) {
+    navigator.clipboard.writeText(item.innerText);
+    console.log("Copiado al portapapeles: " + item.innerText);
+}
+
+document.querySelectorAll('.hidden-click-item').forEach(item => {
+    item.addEventListener('click', () => (showClicked(item.lastElementChild)))
+});
+
+document.querySelectorAll('.has-hidden-item').forEach(item => {
+    item.addEventListener('mouseover',  ()=>{showHidden(item.lastElementChild)});
+    item.addEventListener('mouseout',  ()=>{hide(item.lastElementChild)});
+})
+
+function showClicked(element){
+    element.setAttribute("style", "display:block");
+    click.setAttribute("style", "display:none");
+    setTimeout(function () {
+        element.setAttribute("style", "display:none");
+        click.setAttribute("style", "display:flex");
+    }, 1000);
+}
+
+function showHidden(element) {
+    element.setAttribute("style", "display:block;");
+    hover.setAttribute("style", "display:none;");
+    
+}
+
+function hide(element) {
+    element.setAttribute("style", "display:none;")
+    if(i < 15) {
+        hover.setAttribute("style", "display:flex;");
+        i++;
+    }
+}
